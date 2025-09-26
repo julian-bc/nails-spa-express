e94f791a781d45264e1b39dc284dca705a2c90f8
Julian Enrique <julianbuitragocharry2006@gmail.com>
2025-09-26 17:52:43 -0500

    delete console.log

--- src/middlewares/allowRoles.middleware.js
+++ src/middlewares/allowRoles.middleware.js
@@ -1,9 +1,6 @@
 export const allowRoles = (...allowedRoles) => {
   return (req, res, next) => {
-
     const userRole = req.user.role;
-    console.log(allowRoles);
-    console.log(userRole);
 
     if (!allowedRoles.includes(userRole)) {
       return res.status(403).send({ error: "Access denied, you don't have sufficient privileges to perform this action!" });
