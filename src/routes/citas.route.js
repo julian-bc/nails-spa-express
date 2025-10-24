6fec501113a96a041b46940beaea56a400da0346
unknown <u20231213568@usco.edu.co>
2025-10-24 11:43:45 -0500

    feat: update appointments

--- src/routes/citas.route.js
+++ src/routes/citas.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { 
     createAppointment,
+    updateAppointment,
     getAvailabilityForServiceInLocation,
     getAppointments,
     getAppointmentsByEmployee, 
@@ -16,6 +17,7 @@ router.get("/", authMiddleware, allowRoles("admin", "employee"), getAppointments
 router.get("/employee/:id", authMiddleware, allowRoles("admin", "employee"), getAppointmentsByEmployee);
 router.get("/user/:id", getAppointmentsByUser);
 router.post("/", authMiddleware, allowRoles("admin", "employee"), createAppointment);
+router.put("/:id", authMiddleware, allowRoles("admin", "employee"), updateAppointment);
 router.post("/availability", getAvailabilityForServiceInLocation);
 
 export default router
